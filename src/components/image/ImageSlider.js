f16f223a8dc2ae5ce62f9bff8e4f4ee3e9f1baaa
Sara Scaini <sa.scaini@gmail.com>
2020-01-30 21:55:42 +0000

    fix data-set error as

--- src/components/image/ImageSlider.js
+++ src/components/image/ImageSlider.js
@@ -3,27 +3,35 @@ import React from 'react'
 import AwesomeSlider from 'react-awesome-slider';
 import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
 // import 'react-awesome-slider/dist/styles.css';
-import imageTest1 from '../../images/IMG_4096.jpg'
-import imageTest2 from '../../images/IMG_4211.jpg'
-import imageTest3 from '../../images/main-bg.jpg'
+import imageTest1 from '../../images/img_1.jpg'
+import imageTest2 from '../../images/img_2.jpg'
+import imageTest3 from '../../images/img_3.jpg'
+import imageTest4 from '../../images/img_4.jpg'
 
 
 const ImageSlider = () => {
   const imageList = [
     imageTest1,
     imageTest2,
-    imageTest3
+    imageTest3,
+    imageTest4
   ]
 
-const mapImages = imageList.map( image =>  <div> <img src={`${image}`} alt=""/> </div> )
-console.warn(mapImages)
+const mapImages = imageList.map( (image, index) => (
+  <div>
+    <img data-set={`img_${index + 1}`} src={`${image}`} alt="" />
+  </div>)
+)
+
 const slider = (
-   <AwesomeSlider cssModule={AwesomeSliderStyles}>
-    <div> <img src={imageTest3} alt=""/> </div>
+  <AwesomeSlider cssModule={AwesomeSliderStyles}>
+    {mapImages}
   </AwesomeSlider>
 )
 
-return slider
+return <div>
+  {slider}
+</div>
 
 }
 
