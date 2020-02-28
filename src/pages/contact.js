5d7cc33b56f192267b2d892f1a8d4c7362df043e
Sara Scaini <sa.scaini@gmail.com>
2020-02-28 23:44:52 +0000

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,16 @@
 import React from 'react'
-
+import LeftSideContainer from '../components/LeftSideContainer'
 const Contact = ( ) => {
-return <div>Contact</div>
+return <div className="mainContainer">
+  <LeftSideContainer />
+  <div className="rightContainer">
+    <div className="textContainer">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>Email</p>
+      <p>Phone</p>
+      <p>Social</p>
+    </div>
+  </div>
+</div>
 }
 export default Contact
