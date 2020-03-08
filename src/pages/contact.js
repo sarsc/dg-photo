2489c252f78e18b7f409a10f9684ec5a319b6d20
Sara Scaini <sa.scaini@gmail.com>
2020-03-08 22:57:45 +0000

    fix mess 2

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import LeftSideContainer from '../components/LeftSideContainer'
+
 const Contact = ( ) => {
-return <div className="mainContainer">
-  <LeftSideContainer />
-  <div className="rightContainer">
-    <div className="textContainer">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <p>Email</p>
-      <p>Phone</p>
-      <p>Social</p>
+  return <div className="mainContainer">
+    <LeftSideContainer />
+    <div className="rightContainer">
+      <div className="textContainer">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Email</p>
+        <p>Phone</p>
+        <p>Social</p>
+      </div>
     </div>
   </div>
-</div>
 }
 export default Contact
