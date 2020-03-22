fb2885e5fc742e74ba9a4ac66ad2c7738cab98f1
Sara Scaini <sa.scaini@gmail.com>
2020-03-22 17:18:38 +0000

    add gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,27 +27,7 @@ module.exports = {
         path: `src/assets/images`,
       },
     },
-    // {
-    //   resolve: 'gatsby-transformer-cloudinary',
-    //   options: {
-    //     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    //     apiKey: process.env.CLOUDINARY_API_KEY,
-    //     apiSecret: process.env.CLOUDINARY_API_SECRET,
-    //     uploadFolder: 'gatsby-cloudinary',
-    //     },
-    // // },
-    {
-      resolve:`gatsby-source-cloudinary`,
-      options: {
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        apiKey: process.env.CLOUDINARY_API_KEY,
-        apiSecret: process.env.CLOUDINARY_API_SECRET,
-        resourceType: `image`,
-        type: `type Value`,
-        maxResults: `Max result`,
-        tags:`fetch image tags?`,
-        prefix: `abc-xyz/`
-        },
-    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ]
 }
